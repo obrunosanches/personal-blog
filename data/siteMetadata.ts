caa1ea82db25035e615f0824e9c386a1a013f9b6
Bruno Sanches <bruno.sanches@hotmart.com>
2022-08-20 16:24:07 -0300

    feat: add sitemeta data

---
+++ data/siteMetadata.ts
@@ -0,0 +1,8 @@
+const siteMetadata = {
+  title: 'Blog | Bruno Sanches',
+  description: 'Meu bloco de notas sobre as descobertas e aprendizaados adquiridos através do mundo front end e tecnologias em geral.',
+  siteUrl: 'https://obrunosanches.dev',
+  locale: 'pt-BR',
+}
+
+export default siteMetadata
